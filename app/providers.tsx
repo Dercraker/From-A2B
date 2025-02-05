8a7b07c8bdfdca4b0019de2fa0b5cdb5db578eb6
Dercraker <antoine.capitain@gmail.com>
2025-02-05 17:41:41 +0100

    feat(analytics): add vercel and posthog
    Closes #72

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,13 +4,17 @@ import { GlobalDialogLazy } from "@/components/globalDialog/GlobalDialogLazy";
 import { SearchParamsMessageToastSuspended } from "@/components/searchparams-message/SearchParamsMessageToast";
 import { Toaster } from "@/components/ui/sonner";
 import { AlertDialogRenderer } from "@/features/alert-dialog/AlertDialogRenderer";
+import { env } from "@/lib/env/client";
 import { logger } from "@/lib/logger";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Analytics } from "@vercel/analytics/react";
 import { APIProvider } from "@vis.gl/react-google-maps";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { type PropsWithChildren } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect, type PropsWithChildren } from "react";
 
 const queryClient = new QueryClient({});
 
@@ -18,53 +22,54 @@ type ProvidersProps = PropsWithChildren<{
   GOOGLE_MAPS_JS_API_KEY: string;
 }>;
 
-// if (typeof window !== "undefined") {
-//   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-//     api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
-//     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-//   });
-// }
+if (typeof window !== "undefined") {
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
 
 export const Providers = ({
   children,
   GOOGLE_MAPS_JS_API_KEY,
 }: ProvidersProps) => {
   return (
-    // <PostHogProvider client={posthog}>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <APIProvider
-        apiKey={GOOGLE_MAPS_JS_API_KEY}
-        onLoad={() => logger.debug("Maps api logged")}
-      >
-        <SessionProvider>
-          <QueryClientProvider client={queryClient}>
-            <Toaster />
-            <AlertDialogRenderer />
-            <GlobalDialogLazy />
-            <SearchParamsMessageToastSuspended />
-            <ReactQueryDevtools />
-            {/* <IdentifyUserPosthog /> */}
+    <PostHogProvider client={posthog}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <APIProvider
+          apiKey={GOOGLE_MAPS_JS_API_KEY}
+          onLoad={() => logger.debug("Maps api logged")}
+        >
+          <SessionProvider>
+            <QueryClientProvider client={queryClient}>
+              <Analytics />
+              <Toaster />
+              <AlertDialogRenderer />
+              <GlobalDialogLazy />
+              <SearchParamsMessageToastSuspended />
+              <ReactQueryDevtools />
+              <IdentifyUserPosthog />
 
-            {children}
-          </QueryClientProvider>
-        </SessionProvider>
-      </APIProvider>
-    </ThemeProvider>
-    // </PostHogProvider>
+              {children}
+            </QueryClientProvider>
+          </SessionProvider>
+        </APIProvider>
+      </ThemeProvider>
+    </PostHogProvider>
   );
 };
 
-// const IdentifyUserPosthog = () => {
-//   const session = useSession();
+const IdentifyUserPosthog = () => {
+  const session = useSession();
 
-//   useEffect(() => {
-//     if (!session.data?.user) return;
+  useEffect(() => {
+    if (!session.data?.user) return;
 
-//     posthog.identify(session.data.user.id, {
-//       email: session.data.user.email,
-//       name: session.data.user.name,
-//     });
-//   }, [session.data?.user]);
+    posthog.identify(session.data.user.id, {
+      email: session.data.user.email,
+      name: session.data.user.name,
+    });
+  }, [session.data?.user]);
 
-//   return null;
-// };
+  return null;
+};
